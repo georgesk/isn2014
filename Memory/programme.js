d5f17b174b1649504d2d732464b718aa7c9d7c89
Guillaume Thiry <thiry.guillaume@orange.fr>
2014-04-13 22:29:05 +0200

    # Page hmtl :
    - ajout d'une sortie pour tester les fonctions
    - ajout de commentaires
    - quelques modifications mineures
    
    # Programme js :
    
    - Ajout de commentaires dans le programme
    - Ajout d'une fonction sortie affichant la liste finie des nombres triés
    
    # Theme css :
    
    - Quelques modifications mineures

--- Memory/programme.js
+++ Memory/programme.js
@@ -22,11 +22,14 @@ function putText(id, newText){
  **/
 
 function Rand(){
-	var liste= new Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24);
-	var tab = new Array();
+	var liste= new Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24); //liste de nombres de départ
+	var tab = new Array();  //liste vide des nombres triés
 	a=0;
-	while(liste.length != 0){
-		n = Math.floor((Math.random() * liste.length));
+	while(liste.length != 0){  //tant que liste pas vide
+		n = Math.floor((Math.random() * liste.length));     //choisis un nombre aleatoire
 		tab[a]=liste[n];
+		liste.splice(n,1);
+		a=a+1;  //compteur +
+    putText("zone2", tab); //affichage
 }
 }
