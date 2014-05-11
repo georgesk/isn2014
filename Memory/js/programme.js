a59740202aaad41d2fdd493dbb415e6d2d8980d2
Guillaume Thiry <thiry.guillaume@orange.fr>
2014-05-11 19:06:15 +0200

    Mise en place de l'algorithme de jeu
    #Page Html
    - Rajout d'une zone de texte pour tester
    
    #Fichiers
    - Ajout de 12 images correspondant provisoirement aux 12 paires de cartes
    
    #Programme JavaScript
    - Ajout d'une fonction img()
    - Cette fonction determine la valeur d'une case en fonction de son id et de la liste aleatoire
    - Elle affiche ensuite l'une des douze image en fonction de la valeur de la case
    
    - Ajout d'une fonction compare()
    - Elle determine les deux valeurs des deux cases (id+aleatoire)
    - Si ces deux valeurs sont comprises dans la même paire, les deux cartes sont les mêmes
    - Renvoie alors un "true" indiquant que les cartes doivent rester retournées
    
    - Suppression (temporaire ?) de l'animation fractionnée du retournement de carte
    - Ajout d'un compteur de coup affiché sur la page
    - Ajout de plusieurs variables

--- Memory/js/programme.js
+++ Memory/js/programme.js
@@ -20,10 +20,11 @@ function putText(id, newText){
 /**
  * programme principal
  **/
-
+ 
+var tab = new Array();
 function Rand(){
 	var liste = new Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24); //liste de nombres de départ
-	var tab = new Array();  //liste vide des nombres triés
+	tab = [];           //liste vide des nombres triés
 	a=0;
 	while(liste.length != 0){  //tant que liste pas vide
 		n = Math.floor((Math.random() * liste.length));     //choisis un nombre aleatoire
@@ -34,34 +35,68 @@ function Rand(){
 }
 }
 
-var secund = false;
-var first;
+var secund = false;     //variable indiquant si il s'agit de la 1ere ou 2eme carte
+var first;              //variable enregistrant le code de la 1ere carte (#id)
+var idfirst;            //variable enristrant l'id de la 1ere carte
+var find = false;       //booleen indiquant si le coup est gagné ou non
+var coup = 0;           //compteur du nombre de coup
+
 function hh(a){
-    if (secund==true){   //test si premier ou deuxieme clic
-        $(a).attr('src', 'hh.png');    //changer image de la case a
+    if (secund==true){      //test si premier ou deuxieme clic
+        img(a);     //execute la fonction img() sur a
         secund=false;
-        setTimeout(   //timer de 500 ms
-            function(){     //remise à zero des images
-                $(first).attr('src', 'img.png');
-                $(a).attr('src', 'img.png');
-            }, 
-            500
-        );
+        compare(a);     //execute la fonction compare()
+        coup=coup+1;    //compteur de coup
+        putText("result", coup);
+        if (find==false){       //verifie que 2 cartes sont identiques ou non
+            setTimeout(         //timer de 500 ms
+                function(){
+                    $(first).attr('src', 'img.png');
+                    $(a).attr('src', 'img.png');
+                }, 
+                500
+            );
+        }else{
+            find=false;
+        }
     }
     else{
         first="#";
-        $(a).attr('src', '1.png');      //debut d'une suite de modifications avec timer = animations 
-        setTimeout(
-            function(){$(a).attr('src', '2.png')},200
-            );
-        setTimeout(
-            function(){$(a).attr('src', '3.png')},200
-            );
-        setTimeout(
-            function(){$(a).attr('src', 'hh.png')},200
-            );
-        first = first +  $(a).attr('id');   //enregistrement id de la premiere case cliquee
+        img(a);
+        first = first +  $(a).attr('id');       //code de la premiere carte
+        idfirst = $(a).attr('id');      //garde l'id de la premiere carte
         secund=true;
     }
 
+}
+
+function img(a){            //fonction d'affichage de la carte retournee
+    var id = $(a).attr('id');
+    var val = ((tab[id-1])+(tab[id-1]%2))/2;        //calcule valeur pour l'id en fonction de la liste aleatoire
+    if (val==1){$(a).attr('src', 'zz1.png');}       //associe à chaque valeur une image
+    if (val==2){$(a).attr('src', 'zz2.png');}
+    if (val==3){$(a).attr('src', 'zz3.png');}
+    if (val==4){$(a).attr('src', 'zz4.png');}
+    if (val==5){$(a).attr('src', 'zz5.png');}
+    if (val==6){$(a).attr('src', 'zz6.png');}
+    if (val==7){$(a).attr('src', 'zz7.png');}
+    if (val==8){$(a).attr('src', 'zz8.png');}
+    if (val==9){$(a).attr('src', 'zz9.png');}
+    if (val==10){$(a).attr('src', 'zz10.png');}
+    if (val==11){$(a).attr('src', 'zz11.png');}
+    if (val==12){$(a).attr('src', 'zz12.png');}   
+}
+
+function compare(a){            //fonction qui compare la 1ere et 2eme 
+    var id = $(a).attr('id');   
+    if (tab[id-1]%2==0){        //determine si id est pair ou non
+        if (tab[id-1]==tab[idfirst-1]+1){       //compare les deux valeurs
+            find = true;
+    }
+    }
+    else{
+        if (tab[id-1]==tab[idfirst-1]-1){       //comparaison aussi
+            find = true;
+    }
+    }
 }
